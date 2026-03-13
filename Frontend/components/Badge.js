f947029effb2307eac8b35f110b12cea8e1b024a
CJ2588 <chijioken15@gmail.com>
2026-03-13 13:35:31 -0400

    Further changes to frontend

--- Frontend/components/Badge.js
+++
@@ -1,7 +0,0 @@
-export default function Badge({ label, value }) {
-  return (
-    <div style={{ margin: 5, padding: 8, border: "1px solid #ccc", borderRadius: 5 }}>
-      <strong>{label}:</strong> {value}
-    </div>
-  );
-}
